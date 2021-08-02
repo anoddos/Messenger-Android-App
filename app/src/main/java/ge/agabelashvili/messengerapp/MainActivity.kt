204a9a8139eff41eedaeeae152ed2572a27d61ce
dbejanishvili <48618560+anoddos@users.noreply.github.com>
2021-08-02 20:32:31 +0400

    added sign up page

--- app/src/main/java/ge/agabelashvili/messengerapp/MainActivity.kt
+++ app/src/main/java/ge/agabelashvili/messengerapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package ge.agabelashvili.messengerapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun sign_in(view: View) {}
+
+
+    fun sign_up(view: View) {
+
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
