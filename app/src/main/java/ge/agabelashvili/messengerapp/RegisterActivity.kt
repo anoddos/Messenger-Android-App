6d0a1d8b13945c9c84e0354231621abc8c2f8892
dbejanishvili <48618560+anoddos@users.noreply.github.com>
2021-08-05 16:17:24 +0400

    added firebase service auth

--- app/src/main/java/ge/agabelashvili/messengerapp/RegisterActivity.kt
+++ app/src/main/java/ge/agabelashvili/messengerapp/RegisterActivity.kt
@@ -2,10 +2,32 @@ package ge.agabelashvili.messengerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+    }
+
+
+    fun register(view: View) {
+        val email = Name.text.toString()
+        val password = Pass.text.toString()
+        val possition = what_I_Do.text.toString()
+        if( email.isEmpty() || password.isEmpty() || possition.isEmpty() ){
+            Toast.makeText(this, "Please fill in forms", Toast.LENGTH_SHORT)
+            return@register
+        }
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener{
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+            }
     }
 }
